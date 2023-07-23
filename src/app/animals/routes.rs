c5f3afbeee11a12a5a669bed28c52b3fe9637af0
dreadster3 <afonso.antunes@live.com.pt>
2023-07-23 21:39:08 +0100

    Fix normalize path

--- src/app/animals/routes.rs
+++ src/app/animals/routes.rs
@@ -1,15 +1,14 @@
 use actix_web::{get, HttpResponse, Responder};
 use log::info;
 
-#[get("/")]
+#[get("")]
 pub async fn index() -> impl Responder {
     info!("Hello from animals::routes::index()!");
     return HttpResponse::Ok().body("Hello world!");
 }
 
 pub fn register_routes(cfg: &mut actix_web::web::ServiceConfig) {
-    let scope = actix_web::web::scope("animals")
-        .service(index);
+    let scope = actix_web::web::scope("animals").service(index);
 
     cfg.service(scope);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 mod app;
 
-use actix_web::{HttpServer, App};
-use actix_web::middleware::Logger;
+use actix_web::middleware::{Logger, NormalizePath};
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let log_env = env_logger::Env::new()
-                    .default_filter_or("info");
+    let log_env = env_logger::Env::new().default_filter_or("info");
 
     env_logger::init_from_env(log_env);
 
-    return HttpServer::new( || {
+    return HttpServer::new(|| {
         return App::new()
             .wrap(Logger::default())
+            .wrap(NormalizePath::trim())
             .configure(app::routes::register_routes);
     })
     .bind(("127.0.0.1", 8080))?
-    .run().await;
+    .run()
+    .await;
 }
